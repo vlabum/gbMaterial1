44c943c5cf2f36ea625e1ae81b47ffc2578dfb30
vlabu <vlabum@gmail.com>
2019-05-26 11:04:42 +0500

    BottomNavigationView, fab animation menu

---
+++ app/src/main/java/ru/vlabum/android/gb/material1/Fragment2.kt
@@ -0,0 +1,32 @@
+package ru.vlabum.android.gb.material1
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment2 : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment2_main, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+}
--- app/src/main/java/ru/vlabum/android/gb/material1/MainActivity.kt
+++ app/src/main/java/ru/vlabum/android/gb/material1/MainActivity.kt
@@ -1,74 +1,76 @@
 package ru.vlabum.android.gb.material1
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.bottom_content.*
-import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val LOGD = "DDDDD"
 
-    lateinit var bottomLayout: ViewGroup
-    lateinit var bottomSheetBehavior: BottomSheetBehavior<ViewGroup>
-    lateinit var shakeAnim: Animation
+    //    lateinit var bottomLayout: ViewGroup
+//    lateinit var bottomSheetBehavior: BottomSheetBehavior<ViewGroup>
+    lateinit var navView: BottomNavigationView
+
+    private val onNavigationItemSelectedLoader = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_main, Fragment1()).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_main, Fragment2()).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_main, Fragment3()).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        bottomLayout = bottom_layout
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomLayout)
+        navView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedLoader)
 
-        var bottomSheetCallback = object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onSlide(p0: View, p1: Float) {
-                Log.d("$LOGD onSlide", "" + p1)
-            }
-
-            override fun onStateChanged(p0: View, p1: Int) {
-                Log.d("$LOGD onStateChanged", "" + p1)
-            }
-
-        }
-        bottomSheetBehavior.setBottomSheetCallback(bottomSheetCallback)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_main, Fragment1()).commit()
 
         fab.setOnClickListener { view ->
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-        }
+            var rotated = view.rotation != 0f
+            view.animate().rotation(if (rotated) 0f else 45f)
 
-        shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake)
-
-        bt_login.setOnClickListener {
-            clickBtLogin()
-        }
-
-    }
-
-    fun clickBtLogin() {
-        var isError = false
-        et_username.text?.let { t ->
-            if (t.toString().equals("")) {
-                et_username.error = "username is empty"
-                isError = true
+            if (rotated) {
+                fab1.hide()
+                fab2.hide()
+                fab3.hide()
+            } else {
+                fab1.show()
+                fab2.show()
+                fab3.show()
             }
+
+            fab1.animate()
+                .alpha(if (rotated) 0f else 1f)
+                .translationY(if (rotated) 0f else -fab1.height.toFloat() - 8f)
+            fab2.animate()
+                .alpha(if (rotated) 0f else 1f)
+                .translationY(if (rotated) 0f else 2 * (-fab2.height.toFloat() - 8f))
+            fab3.animate()
+                .alpha(if (rotated) 0f else 1f)
+                .translationY(if (rotated) 0f else 3 * (-fab3.height.toFloat() - 8f))
         }
-        et_password.text?.let { t ->
-            if (t.toString().equals("")) {
-                et_password.error = "password is empty"
-                isError = true
-            }
-        }
-        if (isError) bt_login.startAnimation(shakeAnim)
+
+        fab1.hide()
+        fab2.hide()
+        fab3.hide()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
