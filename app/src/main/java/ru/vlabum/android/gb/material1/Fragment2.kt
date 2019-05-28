ec1307d8ecfccda170f5da196137ec4199e5beaa
vlabu <vlabum@gmail.com>
2019-05-28 21:34:38 +0500

    RecyclerView different types

--- app/src/main/java/ru/vlabum/android/gb/material1/Fragment2.kt
+++ app/src/main/java/ru/vlabum/android/gb/material1/Fragment2.kt
@@ -6,11 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import layout.UserRecyclerAdapter
 
 class Fragment2 : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var recyclerView: RecyclerView? = view?.findViewById(R.id.recyclier_fragment2)
+        recyclerView?.layoutManager = LinearLayoutManager(view?.context)
+        recyclerView?.adapter = UserRecyclerAdapter(generateUsers())
     }
 
     override fun onCreateView(
@@ -27,6 +37,21 @@ class Fragment2 : Fragment() {
 
     override fun onDetach() {
         super.onDetach()
+    }
+
+    private fun generateUsers(): List<User> {
+        var users = ArrayList<User>()
+        context?.resources?.let { res ->
+            for (i in 0 until 20) {
+                when (i % 5 == 0) {
+                    true ->
+                        users.add(Friend(res.getIdentifier("p${i % 10}", "drawable", context?.packageName), "Vasya $i"))
+                    else ->
+                        users.add(User(res.getIdentifier("p${i % 10}", "drawable", context?.packageName), "Vasya $i"))
+                }
+            }
+        }
+        return users
     }
 
 }
---
+++ app/src/main/java/ru/vlabum/android/gb/material1/User.kt
@@ -0,0 +1,4 @@
+package ru.vlabum.android.gb.material1
+
+open class User(var avatarID: Int, var name: String) {
+}
